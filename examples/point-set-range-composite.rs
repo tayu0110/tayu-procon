6a4fef33f3eeada1456a6745974525a81fb151b1
tayu <tayu.dev@gmail.com>
2022-10-15 02:31:24 +0900

    Improved segtree::SegmentTree so that it can be folded from either the left or right side.

--- examples/point-set-range-composite.rs
+++ examples/point-set-range-composite.rs
@@ -1,43 +1,5 @@
-// https://judge.yosupo.jp/problem/point_add_range_sum
-// use tayu_procon::{
-//     scan,
-//     segtree::SegmentTree
-// };
-
-// fn main() {
-//     scan!(n: usize, q: usize, a: [usize; n], p: [(usize, usize, usize); q]);
-
-//     let mut st = SegmentTree::new(n, 0, |l, r| *l + *r);
-//     for (i, v) in a.into_iter().enumerate() {
-//         st.set(i, v);
-//     }
-
-//     for (t, l, r) in p {
-//         if t == 0 {
-//             st.update_by(l, r, |old, val| *old + *val);
-//         } else {
-//             println!("{}", st.get(l, r));
-//         }
-//     }
-// }
-
-fn main() {
-    scan!(n: usize, q: usize, a: [usize; n], p: [(usize, usize, usize); q]);
-
-    let mut st = SegmentTree::new(n, 0, |l, r| *l + *r);
-    for (i, v) in a.into_iter().enumerate() {
-        st.set(i, v);
-    }
-
-    for (t, l, r) in p {
-        if t == 0 {
-            st.update_by(l, r, |old, val| *old + *val);
-        } else {
-            println!("{}", st.get(l, r));
-        }
-    }
-}
-
+// https://judge.yosupo.jp/submission/108278
+mod iolib {
 use std::cell::RefCell;
 use std::collections::VecDeque;
 use std::io::{
@@ -123,7 +85,7 @@ macro_rules! scan {
     };
     // Expand tuple
     ( @interactive : $interactive:literal, @expandtuple, ( $t:tt )) => {
-        { let tmp = $crate::scan_string($interactive).parse::<$t>().unwrap(); tmp }
+        { let tmp = $crate::iolib::scan_string($interactive).parse::<$t>().unwrap(); tmp }
     };
     // Expand tuple
     ( @interactive : $interactive:literal, @expandtuple, ( $t:tt $( , $rest:tt )* ) ) => {
@@ -138,7 +100,7 @@ macro_rules! scan {
     };
     // let $v: $t = ......
     ( @interactive : $interactive:literal, $v:ident : $t:ty ) => {
-        let $v = $crate::scan_string($interactive).parse::<$t>().unwrap();
+        let $v = $crate::iolib::scan_string($interactive).parse::<$t>().unwrap();
     };
     // let $v: $t = ......, .......
     ( @interactive : $interactive:literal, $v:ident : $t:ty, $( $rest:tt )+ ) => {
@@ -157,6 +119,7 @@ macro_rules! scani {
         $crate::scan!(@interactive: true, $( $rest )*);
     };
 }
+}
 
 pub struct SegmentTree<M>
 where M: Clone {
@@ -168,9 +131,26 @@ where M: Clone {
 
 impl<M> SegmentTree<M>
 where M: Clone {
+    /// * `size` - Number of elements in the data array to be managed
+    /// * `e`    - Identity element of the monoid that the data represents
+    /// * `op`   - Operation applied to data (If the operation is to fold the data toward the right, op(a, b) must return b*a as the result)
     #[inline]
     pub fn new(size: usize, e: M, op: fn(&M, &M) -> M) -> Self {
         let tree = vec![e.clone(); size << 1];
+        Self { size, e, op, tree }
+    }
+
+    /// * `vec`  - Initial state of the data array to be managed
+    /// * `e`    - Identity element of the monoid that the data represents
+    /// * `op`   - Operation applied to data (If the operation is to fold the data toward the right, op(a, b) must return b*a as the result)
+    pub fn from_vec(vec: &Vec<M>, e: M, op: fn(&M, &M) -> M) -> Self {
+        let size = vec.len();
+        let mut tree = [vec![e.clone(); size], vec.clone()].concat();
+
+        for i in (0..(size<<1)-1).rev().step_by(2).take_while(|i| i>>1 > 0) {
+            tree[i>>1] = op(&tree[i], &tree[i|1]);
+        }
+
         Self { size, e, op, tree }
     }
 
@@ -184,11 +164,11 @@ where M: Clone {
         self.tree[index] = f(&self.tree[index], &val);
         while index >> 1 > 0 {
             index >>= 1;
-            self.tree[index] = self.op(&self.tree[index<<1], &self.tree[index<<1|1]);
+            self.tree[index] = self.op(&self.tree[index<<1], &self.tree[index<<1|1], false);
         }
     }
-    
-    pub fn get(&self, left: usize, right: usize) -> M {
+
+    fn fold(&self, left: usize, right: usize, fold_right: bool) -> M {
         if left >= right {
             return self.e.clone();
         }
@@ -197,20 +177,54 @@ where M: Clone {
         let (mut res_l, mut res_r) = (self.e.clone(), self.e.clone());
         while l < r {
             if l & 1 != 0 {
-                res_l = self.op(&self.tree[l], &res_l);
+                res_l = self.op(&self.tree[l], &res_l, fold_right);
                 l += 1;
             }
             if r & 1 != 0 {
-                r -= 1;
-                res_r = self.op(&res_r, &self.tree[r]);
+                res_r = self.op(&res_r, &self.tree[r-1], fold_right);
             }
             l >>= 1;
             r >>= 1;
         }
 
-        self.op(&res_l, &res_r)
+        self.op(&res_l, &res_r, false)
+    }
+    
+    /// Fold the operation in a leftward direction.
+    /// That is, you obtain op(t_{l}, op(t_{l+1}, op(t_{l+2}, ...op(t_{r-2}, t_{r-1})...))) as a result.
+    #[inline]
+    pub fn foldl(&self, left: usize, right: usize) -> M {
+        self.fold(left, right, false)
+    }
+
+    /// Fold the operation in a rightward direction.
+    /// That is, you obtain op(op(op(...op(t_{l}, t_{l+1}), t_{l+2}), ..., t_{r-2}), t_{r_1}) as a result.
+    #[inline]
+    pub fn foldr(&self, left: usize, right: usize) -> M {
+        self.fold(left, right, true)
     }
 
     #[inline]
-    fn op(&self, lhs: &M, rhs: &M) -> M { let op = self.op; op(&lhs, &rhs) }
+    fn op(&self, lhs: &M, rhs: &M, fold_right: bool) -> M { if !fold_right { (self.op)(&lhs, &rhs) } else { (self.op)(&rhs, &lhs) } }
+}
+
+fn main() {
+    use std::io::Write;
+    let out = std::io::stdout();
+    let mut out = std::io::BufWriter::new(out.lock());
+
+    const MOD: usize = 998244353;
+    scan!(n: usize, q: usize, p: [(usize, usize); n], q: [(usize, usize, usize, usize); q]);
+
+    let mut st = SegmentTree::from_vec(&p, (1, 0), |&(a1, b1), &(a2, b2)| (a1*a2 % MOD, (a2*b1+b2) % MOD));
+
+    for (t, l, r, x) in q {
+        if t == 0 {
+            st.set(l, (r, x));
+        } else {
+            let (a, b) = st.foldr(l, r);
+            // println!("{}", (a * x + b) % MOD);
+            writeln!(out, "{}", (a * x + b) % MOD).ok();
+        }
+    }
 }
