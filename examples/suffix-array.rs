f2962dbbf5a10ec61cfb238057ca78c3660f3292
tayu <tayu.dev@gmail.com>
2022-10-16 16:58:40 +0900

    Optimize utility::SuffixArray

--- examples/suffix-array.rs
+++ examples/suffix-array.rs
@@ -175,21 +175,35 @@ enum Type {
 
 pub struct SuffixArray {
     s: Vec<u32>,
-    sa: Vec<usize>
+    sa: Vec<u32>
 }
 
 impl SuffixArray {
     const CHARS: usize = 1 << 8;
+    const THRESHOLD_NAIVE: usize = 10;
 
     pub fn new(s: impl Into<String>) -> Self {
-        let s = [s.into().bytes().map(|b| b as u32).collect(), vec![0]].concat();
-        let mut sa = vec![std::usize::MAX; s.len()];
+        let s = s.into().bytes().map(|b| b as u32).chain(vec![0].into_iter()).collect::<Vec<_>>();
+        let mut sa = vec![std::u32::MAX; s.len()];
         Self::sa_is(Self::CHARS, &s, &mut sa);
 
         Self { s, sa }
     }
 
-    fn sa_is(kinds: usize, s: &[u32], sa: &mut Vec<usize>) {
+    #[inline]
+    fn sa_naive(s: &[u32], sa: &mut [u32]) {
+        sa.iter_mut().take(s.len()).enumerate().for_each(|(i, v)| *v = i as u32);
+        sa[0..s.len()].sort_by_key(|i| &s[*i as usize..]);
+    }
+
+    fn sa_is(kinds: usize, s: &[u32], sa: &mut [u32]) {
+        if s.len() <= Self::THRESHOLD_NAIVE {
+            Self::sa_naive(s, sa);
+            return;
+        }
+
+        let mut lms_prev = std::u32::MAX;
+        let mut lms_next = vec![std::u32::MAX; s.len()];
         let mut types = vec![Type::S; s.len()];
         let mut lms_indices = vec![vec![]; kinds];
         let mut char_num = vec![0u32; kinds];
@@ -208,117 +222,133 @@ impl SuffixArray {
                 if types[i+1] == Type::S {
                     types[i+1] = Type::LMS;
                     lms_indices[*nc as usize].push(i as u32 + 1);
+                    lms_next[i+1] = lms_prev;
+                    lms_prev = i as u32 + 1;
                 }
                 Type::L
             } else {
-                // if s[i+1] := x, and s[i] := ax := a{s[i+1]}
-                // if c != nc, then x[0] == a. so, s[i+1] := x := a{s[i+2]}
-                // so s[i] <> s[i+1] == a{s[i+1]} <> a{s[i+2]}
-                //      if s[i+1] < s[i+2] (types[i+1] = S), then s[i] < s[i+1] and types[i] = S
-                //      otherwise s[i+1] > s[i+2] (types[i+1] = L), then s[i] > s[i+1] and types[i] = L
                 types[i+1]
-            }
+            };
         }
 
-        let char_start = [vec![0], char_num.iter().scan(0, |s, n| { *s += *n; Some(*s) }).collect()].concat();
+        let char_start = vec![0].into_iter().chain(char_num.iter().scan(0, |s, n| { *s += n; Some(*s) })).collect::<Vec<_>>();
 
         // Calculate Pseudo SA
         Self::induced_sort(&lms_indices.into_iter().flatten().collect::<Vec<_>>(), &char_start, &char_num, s, &types, sa);
 
         let mut rank = 0;
         let mut lms_prev = (std::usize::MAX, std::usize::MAX);
-        let mut lms_perm = vec![];
-        let mut lms_indices = vec![std::u32::MAX; s.len()];
-        for index in sa.iter().take(s.len()).filter(|index| types[**index] == Type::LMS) {
+        let mut lms_sorted = vec![];
+        let mut lms_ranks = lms_next;
+        for index in sa.iter().take(s.len()).filter(|index| types[**index as usize] == Type::LMS) {
             if lms_prev == (std::usize::MAX, std::usize::MAX) {
-                lms_prev = (*index, *index);
-                lms_indices[*index] = rank;
-                lms_perm.push(*index as u32);
+                lms_prev = (*index as usize, *index as usize);
+                lms_ranks[*index as usize] = rank;
+                lms_sorted.push(*index);
             } else {
-                let (l, mut r) = (*index, *index + 1);
-                while r < types.len() && types[r] != Type::LMS {
-                    r += 1;
-                }
-                    
+                let (l, r) = (*index as usize, lms_ranks[*index as usize] as usize);
                 let (pl, pr) = lms_prev;
                 if pr - pl != r - l || s[pl..pr+1] != s[l..r+1] {
                     rank += 1;
                     lms_prev = (l, r);
                 }
-                lms_indices[l] = rank;
-                lms_perm.push(l as u32);
+                lms_ranks[l] = rank;
+                lms_sorted.push(l as u32);
             }
         }
 
-        let lms_indices = if lms_perm.len() as u32 == rank + 1 {
-            lms_perm
+        let lms_indices = if lms_sorted.len() as u32 == rank + 1 {
+            lms_sorted
         } else {
-            let (restore_index, lms_ranks) = lms_indices
+            let (restore_index, new_s) = lms_ranks
                     .into_iter()
                     .enumerate()
                     .filter(|(_, c)| *c != std::u32::MAX)
                     .map(|(i, c)| (i as u32, c))
                     .unzip::<u32, u32, Vec<u32>, Vec<u32>>();
-            Self::sa_is(rank as usize + 1, &lms_ranks, sa);
+            Self::sa_is(rank as usize + 1, &new_s, sa);
             sa.into_iter()
-                .take(lms_ranks.len())
-                .map(|i| restore_index[*i])
+                .take(new_s.len())
+                .map(|i| restore_index[*i as usize])
                 .collect()
         };
 
         Self::induced_sort(&lms_indices, &char_start, &char_num, s, &types, sa);
     }
 
-    fn induced_sort(lms_indices: &[u32], char_start: &[u32], char_num: &[u32], s: &[u32], types: &[Type], sa: &mut Vec<usize>) {
+    #[inline]
+    fn induced_sort(lms_indices: &[u32], char_start: &[u32], char_num: &[u32], s: &[u32], types: &[Type], sa: &mut [u32]) {
         let kinds = char_start.len();
-        sa.iter_mut().take(s.len()).for_each(|sa| *sa = std::usize::MAX);
-        sa[0] = s.len() - 1;
+        sa[0] = s.len() as u32 - 1;
 
+        let mut backet_index = 1;
         let mut checked = 1;
         let mut filled = vec![0; kinds];
         for (i, lms) in lms_indices.into_iter().enumerate() {
-            let c = s[*lms as usize];
+            let c = s[*lms as usize] as usize;
 
-            if i > 0 && c != s[lms_indices[i-1] as usize] {
-                let target = char_start[c as usize] + char_num[c as usize] - 1;
+            if i > 0 && c != s[lms_indices[i-1] as usize] as usize {
+                let target = char_start[c] as usize + char_num[c] as usize - 1;
 
-                while checked <= target {
-                    if sa[checked as usize] != std::usize::MAX && sa[checked as usize] > 0 && types[sa[checked as usize]-1] == Type::L {
-                        let nc = s[sa[checked as usize]-1];
-                        sa[(char_start[nc as usize] + filled[nc as usize]) as usize] = sa[checked as usize] - 1;
-                        filled[nc as usize] += 1;
+                while checked < target {
+                    let mut rem = filled[backet_index];
+                    while rem > 0 {
+                        if sa[checked] != std::u32::MAX && sa[checked] > 0 && types[sa[checked] as usize - 1] == Type::L {
+                            let nc = s[sa[checked] as usize - 1] as usize;
+                            sa[(char_start[nc] + filled[nc]) as usize] = sa[checked] - 1;
+                            filled[nc] += 1;
+                            if nc == backet_index {
+                                rem += 1;
+                            }
+                        }
+                        rem -= 1;
+                        checked += 1;
                     }
 
-                    checked += 1;
+                    checked = char_start[backet_index] as usize + char_num[backet_index] as usize;
+                    filled[backet_index] = 0;
+                    backet_index += 1;
                 }
             }
 
             if types[*lms as usize - 1] == Type::L {
-                let nc = s[*lms as usize - 1];
-                sa[(char_start[nc as usize] + filled[nc as usize]) as usize] = *lms as usize - 1;
-                filled[nc as usize] += 1;
+                let nc = s[*lms as usize - 1] as usize;
+                sa[(char_start[nc] + filled[nc]) as usize] = *lms - 1;
+                filled[nc] += 1;
             }
         }
 
-        for i in checked as usize..s.len() {
-            if sa[i] != std::usize::MAX && sa[i] > 0 && types[sa[i]-1] == Type::L {
-                let c = s[sa[i]-1];
-                sa[(char_start[c as usize] + filled[c as usize]) as usize] = sa[i] - 1;
-                filled[c as usize] += 1;
+        while checked < s.len() {
+            let mut rem = filled[backet_index];
+            while rem > 0 {
+                if sa[checked] != std::u32::MAX && sa[checked] > 0 && types[sa[checked] as usize - 1] == Type::L {
+                    let nc = s[sa[checked] as usize - 1] as usize;
+                    sa[(char_start[nc] + filled[nc]) as usize] = sa[checked] - 1;
+                    filled[nc] += 1;
+                    if nc == backet_index {
+                        rem += 1;
+                    }
+                }
+                
+                rem -= 1;
+                checked += 1;
             }
+
+            checked = char_start[backet_index] as usize + char_num[backet_index] as usize;
+            filled[backet_index] = 0;
+            backet_index += 1;
         }
 
-        let mut filled = vec![0; kinds];
         for i in (0..s.len()).rev() {
-            if sa[i] != std::usize::MAX && sa[i] > 0 && types[sa[i]-1] != Type::L {
-                let c = s[sa[i]-1];
+            if sa[i] != std::u32::MAX && sa[i] > 0 && types[sa[i] as usize - 1] != Type::L {
+                let c = s[sa[i] as usize - 1];
                 sa[(char_start[c as usize] + char_num[c as usize] - 1 - filled[c as usize]) as usize] = sa[i] - 1;
                 filled[c as usize] += 1;
             }
         }
     }
 
-    pub fn get_sa(&self) -> &[usize] {
+    pub fn get_sa(&self) -> &[u32] {
         &self.sa[1..]
     }
 
@@ -326,7 +356,7 @@ impl SuffixArray {
     pub fn lcp_array(&self) -> Vec<usize> {
         let mut rank = vec![0; self.sa.len()-1];
         for (i, sa) in self.sa.iter().enumerate().skip(1) {
-            rank[*sa] = i;
+            rank[*sa as usize] = i;
         }
 
         let mut lcp = 0;
@@ -338,7 +368,7 @@ impl SuffixArray {
             }
         
             let (pos_l, pos_r) = (self.sa[index], self.sa[index+1]);
-            while self.s[lcp + pos_l] == self.s[lcp + pos_r] {
+            while self.s[lcp + pos_l as usize] == self.s[lcp + pos_r as usize] {
                 lcp += 1;
             }
             lcpa[index-1] = lcp;
