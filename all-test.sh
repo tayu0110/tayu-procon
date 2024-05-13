4c8eb950dd3106f245df9e3055d66bfd535b0147
tayu <tayu.dev@gmail.com>
2024-05-14 01:01:51 +0900

    Remove suffix-array from Cargo.toml and all-test.sh

--- all-test.sh
+++ all-test.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 for dir in \
-  "bitset" "complex" "convolution" "fenwick-tree" "flow" "geometry" \
-  "graph" "iolib" "math" "matrix" "mincost-flow" "modint" "numeric" \
-  "polynomial" "rational" "segtree" "simple-rand" "simple-test" "string" "suffix-array" \
+  "balanced-bst" "bitset" "complex" "convolution" "ds" "fenwick-tree" "flow" "geometry" \
+  "graph" "iolib" "math" "matrix" "mincost-flow" "modint" "number-theoretic-transform" "numeric" \
+  "polynomial" "rational" "segtree" "simple-rand" "string" \
   "trie" "two-sat" "unionfind" "utility" "wavelet-matrix"; do
   cd "${dir}" || exit
   echo "${dir} is testing..."
-  cargo test
-  cargo test --doc
+  cargo test --all-features
+  cargo test --release --all-features
   cd ../
 done
