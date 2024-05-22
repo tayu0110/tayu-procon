a1f2194c4cf56324b9701425a6f7842588b917cf
tayu <tayu.dev@gmail.com>
2024-05-23 01:38:47 +0900

    Remove benchmarks from ds::tests::ring_queue.rs

--- ds/tests/ring_queue.rs
+++ ds/tests/ring_queue.rs
@@ -1,41 +1,4 @@
-#![feature(test)]
-extern crate test;
-
 use ds::FixedRingQueue;
-use rand::{thread_rng, Rng};
-use std::{collections::VecDeque, sync::Mutex};
-use test::Bencher;
-
-const SIZE: usize = 1 << 20;
-static QUEUE: Mutex<FixedRingQueue<usize, SIZE>> = Mutex::new(FixedRingQueue::new());
-
-#[bench]
-fn ring_queue_bench(b: &mut Bencher) {
-    let mut rng = thread_rng();
-    let mut nt = QUEUE.lock().unwrap();
-
-    b.iter(|| {
-        for _ in 0..SIZE {
-            nt.push(rng.gen::<usize>());
-        }
-
-        while nt.pop().is_some() {}
-    });
-}
-
-#[bench]
-fn vec_deque_bench(b: &mut Bencher) {
-    let mut rng = thread_rng();
-    let mut nt = VecDeque::new();
-
-    b.iter(|| {
-        for _ in 0..SIZE {
-            nt.push_back(rng.gen::<usize>());
-        }
-
-        while nt.pop_front().is_some() {}
-    });
-}
 
 #[test]
 fn ring_queue_test() {
