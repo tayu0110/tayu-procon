4300b63e9d4c7615759f56dc07c6487efd1a94d2
tayu <tayu.dev@gmail.com>
2024-05-30 22:00:00 +0900

    Remove wavelet-matrix crate from all-test.sh

--- all-test.sh
+++ all-test.sh
@@ -4,7 +4,7 @@ for dir in \
   "balanced-bst" "bitset" "complex" "convolution" "ds" "fenwick-tree" "flow" "geometry" \
   "graph" "iolib" "math" "matrix" "mincost-flow" "modint" "number-theoretic-transform" "numeric" \
   "polynomial" "rational" "segtree" "simple-rand" "string" \
-  "trie" "two-sat" "unionfind" "utility" "wavelet-matrix"; do
+  "trie" "two-sat" "unionfind" "utility"; do
   cd "${dir}" || exit
   echo "${dir} is testing..."
   cargo test --all-features
