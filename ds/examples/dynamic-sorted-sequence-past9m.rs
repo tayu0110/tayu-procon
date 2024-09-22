a515e20da6c62897670667c283f318bf5f11bbf1
tayu0110 <tayu.dev@gmail.com>
2024-09-23 01:37:45 +0900

    Improve ds/examples/dynamic-sorted-sequence-past9m.rs

--- ds/examples/dynamic-sorted-sequence-past9m.rs
+++ ds/examples/dynamic-sorted-sequence-past9m.rs
@@ -1,24 +1,26 @@
+use std::borrow::Cow;
+
 use cpio::*;
 use ds::{DynamicSortedSequence, MapMonoid};
 
 #[derive(Clone)]
 struct T;
 impl MapMonoid for T {
-    type M = (String, usize);
+    type M = (Cow<'static, str>, usize);
     type Act = ();
 
     fn e() -> Self::M {
-        ("".to_owned(), usize::MAX)
+        ("".into(), usize::MAX)
     }
     fn op(_: &Self::M, _: &Self::M) -> Self::M {
-        ("".to_owned(), usize::MAX)
+        ("".into(), usize::MAX)
     }
 
     fn id() -> Self::Act {}
     fn composite(_: &Self::Act, _: &Self::Act) -> Self::Act {}
 
     fn map(_: &Self::M, _: &Self::Act) -> Self::M {
-        ("".to_owned(), usize::MAX)
+        ("".into(), usize::MAX)
     }
 }
 
@@ -28,16 +30,19 @@ fn main() {
     let mut seq = s
         .into_iter()
         .enumerate()
-        .map(|(i, s)| (s, i))
+        .map(|(i, s)| (s.into(), i))
         .collect::<DynamicSortedSequence<T>>();
     for _ in 0..q {
         scan! {x: usize, t: String}
 
         let (_, index) = seq.remove(x - 1);
-        seq.insert((t, index));
+        seq.insert((t.into(), index));
     }
 
-    let mut seq = seq.into_iter().collect::<Vec<_>>();
+    let mut seq = seq
+        .into_iter()
+        .map(|s| (s.0.as_ref().to_owned(), s.1))
+        .collect::<Vec<_>>();
     seq.sort_by_key(|s| s.1);
     putln!(seq.into_iter().map(|v| v.0), @sep = " ");
 }
